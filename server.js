6a0be336eedf250de7f2540b2c91dc5d06d46565
Tim Reed <29209706+11reed@users.noreply.github.com>
2020-11-05 12:42:58 -0500

    commit

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-const path = require("path");
-const express = require("express");
-const app = require("./public/build/bundle");
+const { createServer } = require("http");
+const app = require("./src/App.js");
 
-const server = express();
-
-server.use(express.static(path.join(__dirname, "public")));
-
-server.get("*", function(req, res) {
+createServer((req, res) => {
   const { html } = app.render({ url: req.url });
 
   res.write(`
     <!DOCTYPE html>
-    <link rel='stylesheet' href='/global.css'>
-    <link rel='stylesheet' href='/bundle.css'>
     <div id="app">${html}</div>
-    <script src="/bundle.js"></script>
+    <script src="/build/bundle.js"></script>
   `);
 
   res.end();
-});
-
-const port = 3000;
-server.listen(port, () => console.log(`Listening on port ${port}`));
+}).listen(3000);
