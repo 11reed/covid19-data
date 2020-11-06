3c061c53a5a840c671580a8db49b45e9e66d967c
Tim <reed11tim@gmail.com>
2020-11-05 20:05:03 -0500

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -11,18 +11,18 @@
 
 
 <style>
-	:global(body) {
-		font-family: 'Mukta', sans-serif;
+     :global(body) {
+	font-family: 'Mukta', sans-serif;
         background-color: #212F3D;
-	}
+     }
 
-	div {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-        flex-flow: column;
-        color:#5DADE2 ;
-	}
+     div {
+	 display: flex;
+	 align-items: center;
+	 justify-content: center;
+         flex-flow: column;
+         color:#5DADE2 ;
+     }
 
     h1 {
         font-size: 50px;
