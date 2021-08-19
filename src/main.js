6f984616ab5072268d5ceaab56f2716ba12bcb96
Tim <reed11tim@gmail.com>
2021-08-19 04:05:59 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@ import App from './App.svelte';
 
 const app = new App({
 	target: document.getElementById("app"),
-	props: {
-		name: 'world',
-		hydrate: true
+	props: { 
+		hydrate: true 
 	}
 });
 
